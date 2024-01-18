b060055c372767a93a173a5ef983d5129bd21402
Visal .In <invisal@gmail.com>
2024-01-18 15:29:50 +0700

    fix: fixing the incorrect display of table

--- src/components/result/ResultTable.tsx
+++ src/components/result/ResultTable.tsx
@@ -37,7 +37,7 @@ export default function ResultTable({ data }: ResultTableProps) {
           // />
         );
       }
-      return <></>;
+      return <TextCell value={""} />;
     },
     [data]
   );
