9bd56156f7dda537aa2cb2c7a36249e2cdfe3095
Visal In <invisal@gmail.com>
2024-02-21 06:01:04 +0700

    feat: add cookieless ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,29 @@ export default function RootLayout({
       <head>
         <link rel="prefetch" href="/client" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {process.env.NEXT_PUBLIC_GA && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+            />
+            <Script id="ga">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+
+          gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'analytics_storage': 'denied'
+          });
+
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA}');
+        `}</Script>
+          </>
+        )}
+        {children}
+      </body>
     </html>
   );
 }
