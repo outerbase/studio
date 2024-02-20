167e8c545ebf4eca365f7a4328fc666285409bef
Visal In <invisal@gmail.com>
2024-02-20 14:13:08 +0700

    feat: run current and run all

--- src/components/SqlEditor.tsx
+++ src/components/SqlEditor.tsx
@@ -1,8 +1,11 @@
 import { tags as t } from "@lezer/highlight";
 import { createTheme } from "@uiw/codemirror-themes";
-import CodeMirror from "@uiw/react-codemirror";
+import CodeMirror, {
+  EditorView,
+  ReactCodeMirrorRef,
+} from "@uiw/react-codemirror";
 import { sql, SQLite } from "@codemirror/lang-sql";
-import { KeyboardEventHandler, useMemo } from "react";
+import { forwardRef, KeyboardEventHandler, useMemo } from "react";
 
 import { defaultKeymap } from "@codemirror/commands";
 import { keymap } from "@codemirror/view";
@@ -45,48 +48,63 @@ interface SqlEditorProps {
   onChange: (value: string) => void;
   schema?: Record<string, string[]>;
   onKeyDown?: KeyboardEventHandler<HTMLDivElement>;
+  onCursorChange?: (
+    pos: number,
+    lineNumber: number,
+    columnNumber: number
+  ) => void;
 }
 
-export default function SqlEditor({
-  value,
-  onChange,
-  schema,
-  onKeyDown,
-}: SqlEditorProps) {
-  const keyExtensions = useMemo(() => {
-    return keymap.of([
-      {
-        key: KEY_BINDING.run.toCodeMirrorKey(),
-        preventDefault: true,
-        run: () => true,
-      },
-      ...defaultKeymap,
-    ]);
-  }, []);
+const SqlEditor = forwardRef<ReactCodeMirrorRef, SqlEditorProps>(
+  function SqlEditor(
+    { value, onChange, schema, onKeyDown, onCursorChange }: SqlEditorProps,
+    ref
+  ) {
+    const keyExtensions = useMemo(() => {
+      return keymap.of([
+        {
+          key: KEY_BINDING.run.toCodeMirrorKey(),
+          preventDefault: true,
+          run: () => true,
+        },
+        ...defaultKeymap,
+      ]);
+    }, []);
 
-  return (
-    <CodeMirror
-      autoFocus
-      onKeyDown={onKeyDown}
-      basicSetup={{
-        defaultKeymap: false,
-        drawSelection: false,
-      }}
-      theme={theme}
-      value={value}
-      height="100%"
-      onChange={onChange}
-      style={{
-        fontSize: 20,
-        height: "100%",
-      }}
-      extensions={[
-        keyExtensions,
-        sql({
-          dialect: SQLite,
-          schema,
-        }),
-      ]}
-    />
-  );
-}
+    return (
+      <CodeMirror
+        ref={ref}
+        autoFocus
+        onKeyDown={onKeyDown}
+        basicSetup={{
+          defaultKeymap: false,
+          drawSelection: false,
+        }}
+        theme={theme}
+        value={value}
+        height="100%"
+        onChange={onChange}
+        style={{
+          fontSize: 20,
+          height: "100%",
+        }}
+        extensions={[
+          keyExtensions,
+          sql({
+            dialect: SQLite,
+            schema,
+          }),
+          EditorView.updateListener.of((state) => {
+            const pos = state.state.selection.main.head;
+            const line = state.state.doc.lineAt(pos);
+            const lineNumber = line.number;
+            const columnNumber = pos - line.from;
+            if (onCursorChange) onCursorChange(pos, lineNumber, columnNumber);
+          }),
+        ]}
+      />
+    );
+  }
+);
+
+export default SqlEditor;
