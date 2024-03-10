6fd613c9d5e18305503598c6406da67e5845d88f
Visal .In <invisal@gmail.com>
2024-03-10 23:17:45 +0700

    feat: add github login

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,3 @@
+export default function LogoutPage() {
+  // do something
+}
---
+++ src/db/schema.ts
@@ -0,0 +1,50 @@
+import { sql } from "drizzle-orm";
+import {
+  blob,
+  index,
+  sqliteTable,
+  text,
+  unique,
+} from "drizzle-orm/sqlite-core";
+
+export const user = sqliteTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name"),
+  picture: text("picture"),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const user_session = sqliteTable(
+  "user_session",
+  {
+    id: text("id").primaryKey(),
+    userId: text("user_id")
+      .notNull()
+      .references(() => user.id),
+    authId: text("auth_id"),
+    userAgent: text("user_agent"),
+    expiresAt: blob("expires_at", { mode: "bigint" }).notNull(),
+  },
+  (table) => ({
+    expireIdx: index("user_session_expire_idx").on(table.expiresAt),
+    authIdx: index("user_session_auth_id_idx").on(table.authId),
+  })
+);
+
+export const user_oauth = sqliteTable(
+  "user_auth",
+  {
+    id: text("id").primaryKey(),
+    userId: text("user_id"),
+    provider: text("provider"),
+    providerId: text("provider_id"),
+    createdAt: text("created_at")
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => ({
+    providerUniqueId: unique().on(table.provider, table.providerId),
+  })
+);
