eba9411243538b5054d681981962c9a29c23ab45
Visal In <invisal@gmail.com>
2024-03-13 17:43:59 +0700

    feat: improve the image/link preview

--- src/components/table-cell/display-link-cell.tsx
+++ src/components/table-cell/display-link-cell.tsx
@@ -4,47 +4,47 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "../ui/hover-card";
+import { Button } from "../ui/button";
 
 const IMAGE_EXTENSION = ["jpg", "jpeg", "png", "pneg", "svg", "bmp", "gif"];
 
 export default function DisplayLinkCell({ link }: { link: string }) {
   const extension = link.split(".").pop();
-  console.log("ex", extension);
-
-  if (IMAGE_EXTENSION.includes((extension ?? "").toLowerCase())) {
-    return (
-      <div className="flex">
-        <HoverCard>
-          <HoverCardTrigger
-            href={link}
-            target="_blank"
-            className="text-blue-600 dark:text-blue-300 underline"
-          >
-            {link}
-          </HoverCardTrigger>
-          <HoverCardContent>
-            <div className="p-2 bg-gray-600 dark:bg-gray-800 rounded">
-              <img
-                src={link}
-                alt=""
-                className="object-contain w-[200px] h-[200px]"
-              />
-            </div>
-          </HoverCardContent>
-        </HoverCard>
-      </div>
-    );
-  }
+  const isImage = IMAGE_EXTENSION.includes((extension ?? "").toLowerCase());
 
   return (
     <div className="flex">
-      <Link
-        target="_blank"
-        href={link}
-        className="text-blue-600 dark:text-blue-300 underline"
-      >
-        {link}
-      </Link>
+      <HoverCard>
+        <HoverCardTrigger
+          target="_blank"
+          className="text-blue-600 dark:text-blue-300 underline"
+        >
+          {link}
+        </HoverCardTrigger>
+        <HoverCardContent side="bottom" align="start" className="min-w-[300px]">
+          <div className="flex flex-col gap-2">
+            {isImage && (
+              <div className="p-2 bg-gray-600 dark:bg-gray-800 rounded justify-center items-center flex">
+                <img
+                  src={link}
+                  alt=""
+                  className="object-contain w-[200px] h-[200px]"
+                />
+              </div>
+            )}
+
+            <div className="font-mono text-sm line-clamp-3 truncate break-all w-[250px] whitespace-normal">
+              {link}
+            </div>
+
+            <div>
+              <Link target="_blank" className="inline-block" href={link}>
+                <Button>Open Link</Button>
+              </Link>
+            </div>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
     </div>
   );
 }
