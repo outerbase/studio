dae240e4da92bac3255997bf640b40abaaff2b2b
rin-yato <chearithorn@gmail.com>
2024-03-17 01:05:47 +0700

    chore: add test page

---
+++ src/app/block-editor/page.tsx
@@ -0,0 +1,9 @@
+import { BlockEditor } from "@/components/block-editor";
+
+export default function Page() {
+  return (
+    <div className="m-20 max-w-prose mx-auto">
+      <BlockEditor />
+    </div>
+  );
+}
