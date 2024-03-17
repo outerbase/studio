91e90a7950c005cf178adbe1c422968418a79c04
rin-yato <chearithorn@gmail.com>
2024-03-17 23:53:56 +0700

    chore: remove test page

--- src/app/block-editor/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BlockEditor } from "@/components/block-editor";
-
-export default function Page() {
-  return (
-    <div className="m-20 max-w-prose mx-auto">
-      <BlockEditor />
-    </div>
-  );
-}
