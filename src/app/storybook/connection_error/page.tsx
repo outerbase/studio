30beb040b5250dcbd6f9b200a2ed0ce21e6ff238
Visal .In <invisal@gmail.com>
2024-03-28 12:41:42 +0700

    fixing build error

--- src/app/storybook/connection_error/page.tsx
+++ src/app/storybook/connection_error/page.tsx
@@ -5,7 +5,7 @@ export default function ConnectionErrorMessageStory() {
   return (
     <div className="flex flex-col gap-4">
       <ConnectingDialog message="Authentication failed: The JWT is invalid" />
-      <ConnectingDialog loading url="wss://example.turso.io" />
+      <ConnectingDialog loading />
     </div>
   );
 }
