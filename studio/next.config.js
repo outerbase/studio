d668b86b99ae9a0bb7763d5c13d051700e449c9b
rin-yato <chearithorn@gmail.com>
2024-04-14 19:01:28 +0700

    feat: transpile package for dev

--- studio/next.config.js
+++ studio/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  transpilePackages: ["@libsqlstudio/gui"],
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "@libsqlstudio/gui": "../gui/src/index.tsx",
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
