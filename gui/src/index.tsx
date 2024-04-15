75cc3178e2f1e083be4abe028d9a138fad57fac3
Visal .In <invisal@gmail.com>
2024-04-15 20:58:46 +0700

    remove some part

--- gui/src/index.tsx
+++ gui/src/index.tsx
@@ -8,14 +8,3 @@ export {
   escapeSqlValue,
 } from "./sqlite/sql-helper";
 export { default as parseSafeJson } from "./lib/json-safe";
-export { default as ConnectingDialog } from "./components/connection-dialog";
-export { default as OpacityLoading } from "./components/loading-opacity";
-export { default as QueryProgressLog } from "./components/query-progress-log";
-export {
-  type OptimizeTableRowValue,
-  default as OptimizeTableState,
-} from "./components/table-optimized/OptimizeTableState";
-export type {
-  DatabaseTableColumnChange,
-  DatabaseTableSchemaChange,
-} from "./components/schema-editor";
