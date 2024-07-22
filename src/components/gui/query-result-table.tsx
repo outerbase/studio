d129ba523d3a401412a3ebae0aea3361b1435950
Visal In <invisal@gmail.com>
2024-07-23 08:04:18 +0900

    fix: add line clamp for result header

--- src/components/gui/query-result-table.tsx
+++ src/components/gui/query-result-table.tsx
@@ -75,7 +75,7 @@ function Header({
           }}
         >
           {header.icon ? <div className="mr-2">{header.icon}</div> : null}
-          <div className="grow">{header.displayName}</div>
+          <div className="grow line-clamp-1">{header.displayName}</div>
           <LucideChevronDown className="text-mute w-4 h-4 cursor-pointer" />
         </div>
       </DropdownMenuTrigger>
