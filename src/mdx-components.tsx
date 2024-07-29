2ce1f9d302ddf26c916aea34b58c2f5e07fa91c9
Ave Aristov <chantepkik@gmail.com>
2024-07-29 16:10:44 +0700

    feat: add syntax highlight to mdx code block

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from "mdx/types";
+import CodeBlock from "./components/code-block";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    code: CodeBlock,
   };
 }
