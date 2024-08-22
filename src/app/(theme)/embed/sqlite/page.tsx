0497c0d868ec3c459615e99a25c6f10ad7e14d07
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-08-22 19:27:28 +0300

    feat(embed): override theme variables (#152)

--- src/app/(theme)/embed/sqlite/page.tsx
+++ src/app/(theme)/embed/sqlite/page.tsx
@@ -2,7 +2,11 @@ import ThemeLayout from "../../theme_layout";
 import EmbedPageClient from "./page-client";
 
 export default async function EmbedPage(props: {
-  searchParams: { theme?: string; disableThemeToggle?: string };
+  searchParams: {
+    theme?: string;
+    disableThemeToggle?: string;
+    [key: string]: any;
+  };
 }) {
   let overrideTheme: "dark" | "light" | undefined = undefined;
   const disableToggle = props.searchParams.disableThemeToggle === "1";
@@ -11,8 +15,22 @@ export default async function EmbedPage(props: {
     overrideTheme = props.searchParams.theme === "dark" ? "dark" : "light";
   }
 
+  const overrideThemeVariables: Record<string, string> = {};
+
+  for (const key in props.searchParams) {
+    if (!key.startsWith("themeVariables[")) {
+      continue;
+    }
+
+    overrideThemeVariables[key.slice(15, -1)] = props.searchParams[key];
+  }
+
   return (
-    <ThemeLayout overrideTheme={overrideTheme} disableToggle={disableToggle}>
+    <ThemeLayout
+      overrideTheme={overrideTheme}
+      disableToggle={disableToggle}
+      overrideThemeVariables={overrideThemeVariables}
+    >
       <EmbedPageClient />
     </ThemeLayout>
   );
