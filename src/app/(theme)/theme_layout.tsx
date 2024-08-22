3f11b25d5a1d898c3fc66c90f1816a856b6ca2fb
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-08-22 18:00:26 +0300

    feat(embed): override theme variables

--- src/app/(theme)/theme_layout.tsx
+++ src/app/(theme)/theme_layout.tsx
@@ -13,17 +13,24 @@ export default async function ThemeLayout({
   children,
   overrideTheme,
   disableToggle,
+  overrideThemeVariables,
 }: PropsWithChildren<{
   overrideTheme?: "dark" | "light";
   disableToggle?: boolean;
+  overrideThemeVariables?: Record<string, string>;
 }>) {
   const cookieStore = cookies();
   const theme =
     overrideTheme ??
     (cookieStore.get("theme")?.value === "dark" ? "dark" : "light");
+  const style = overrideThemeVariables ?? {};
 
   return (
-    <body className={cn(inter.className, theme)} suppressHydrationWarning>
+    <body
+      className={cn(inter.className, theme)}
+      style={style}
+      suppressHydrationWarning
+    >
       <ThemeProvider defaultTheme={theme} disableToggle={disableToggle}>
         <Fragment>{children}</Fragment>
         <Toaster />
