7a32c2a562a2f3f9a9b73fba697dace9aa74eded
Visal .In <invisal@gmail.com>
2024-09-02 09:46:08 +0700

    initial highlight code

--- src/components/gui/sql-editor/statement-highlight.ts
+++ src/components/gui/sql-editor/statement-highlight.ts
@@ -12,31 +12,84 @@ const statementLineHighlight = Decoration.line({
   class: "cm-highlight-statement",
 });
 
-function resolveToNearestStatement(node: SyntaxNode, cursor: number) {
-  const statements = node.getChildren("Statement");
-  if (statements.length === 0) return null;
+function checkRequireEndStatement(statement: string) {
+  const prefixList = ["CREATE TRIGGER", "ALTER TRIGGER"];
 
-  for (let i = 0; i < statements.length; i++) {
-    const statement = statements[i];
-    if (statement.to < cursor) continue;
-    return statement;
+  for (const prefix of prefixList) {
+    if (statement.substring(0, prefix.length).toUpperCase() === prefix)
+      return true;
   }
 
-  return statements[statements.length - 1];
+  return false;
+}
+
+function resolveToNearestStatement(
+  state: EditorState,
+  node: SyntaxNode,
+  cursor: number
+) {
+  const statements = node.getChildren("Statement");
+
+  if (statements.length === 0) return [];
+
+  let nodes: SyntaxNode[] = [];
+  let needEndStatement = false;
+  let i = 0;
+
+  for (; i < statements.length; i++) {
+    const statement = statements[i];
+
+    // Check if it is required end statement
+    const nodeString = state.doc.sliceString(statement.from, statement.to);
+    needEndStatement = needEndStatement || checkRequireEndStatement(nodeString);
+
+    if (needEndStatement) {
+      nodes.push(statement);
+    }
+
+    if (statement.to > cursor) {
+      break;
+    }
+
+    if (nodeString.toUpperCase() === "END;") {
+      nodes = [];
+      needEndStatement = false;
+    }
+  }
+
+  console.log(needEndStatement, "sss");
+  if (needEndStatement) {
+    // Loop till we find END statement
+    for (; i < statements.length; i++) {
+      const statement = statements[i];
+      const nodeString = state.doc.sliceString(statement.from, statement.to);
+      nodes.push(statement);
+
+      if (nodeString.toUpperCase() === "END;") {
+        break;
+      }
+    }
+  } else {
+    nodes.push(statements[Math.min(i, statements.length - 1)]);
+  }
+
+  return nodes;
 }
 
 function getDecorationFromState(state: EditorState) {
   const tree = syntaxTree(state);
   const node = resolveToNearestStatement(
+    state,
     tree.topNode,
     state.selection.main.from
   );
 
-  if (!node) return Decoration.none;
+  console.log(node);
+  if (node.length === 0) return Decoration.none;
 
   // Get the line of the node
-  const fromLineNumber = state.doc.lineAt(node.from).number;
-  const toLineNumber = state.doc.lineAt(node.to).number;
+  const fromLineNumber = state.doc.lineAt(node[0].from).number;
+  const toLineNumber = state.doc.lineAt(node[node.length - 1].to).number;
 
   const d: Range<Decoration>[] = [];
   for (let i = fromLineNumber; i <= toLineNumber; i++) {
