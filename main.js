76e0d197767697421051aa850b25908706056612
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-10-19 10:19:29 -0400

    Electron app

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    simpleFullscreen: true,
+    title: 'Outerbase Studio'
+  })
+
+  // Load the app from the local server
+  win.loadURL('http://localhost:3008')
+
+  // Open DevTools (optional)
+  // win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
