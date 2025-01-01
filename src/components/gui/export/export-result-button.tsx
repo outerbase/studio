967f228248740631e2ceb821b167e1059e9ddf00
keppere <2keppere@gmail.com>
2025-01-01 17:01:11 +0700

    fix: handle to close export panel on click outside it

--- src/components/gui/export/export-result-button.tsx
+++ src/components/gui/export/export-result-button.tsx
@@ -3,7 +3,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "../../ui/popover";
 import OptimizeTableState, {
   TableSelectionRange,
 } from "../table-optimized/OptimizeTableState";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { getFormatHandlers } from "@/components/lib/export-helper";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
@@ -116,6 +116,21 @@ export default function ExportResultButton({
     selectionCount.ranges.length > 0 ? 0 : -1
   );
   const [open, setOpen] = useState(false);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      popoverRef.current &&
+      !popoverRef.current.contains(event.target as Node)
+    ) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   const onExportClicked = useCallback(() => {
     if (!format) return;
@@ -227,210 +242,212 @@ export default function ExportResultButton({
   };
 
   return (
-    <Popover open={open}>
-      <PopoverTrigger>
-        <div
-          className={buttonVariants({ variant: "ghost", size: "sm" })}
-          onClick={() => setOpen(!open)}
-        >
-          Export
-        </div>
-      </PopoverTrigger>
-      <PopoverContent className="p-0 min-w[550px] w-[550px]">
-        <div className="p-4">
-          <div className="mb-2 font-bold">Export</div>
-          <div className="p-2 rounded-md mb-1 border-solid border border-gray-200">
-            <small>Export target</small>
-            <div className="p-2">
-              <RadioGroup
-                defaultValue={exportTarget}
-                onValueChange={(e) => {
-                  setExportTarget(e as ExportTarget);
-                }}
-              >
-                <div className="flex items-center  space-x-16">
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="clipboard" />
-                    <small>Copy to clipboard</small>
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value="file" />
-                    <small>Export to file</small>
-                  </div>
-                </div>
-              </RadioGroup>
-            </div>
+    <div ref={popoverRef}>
+      <Popover open={open}>
+        <PopoverTrigger>
+          <div
+            className={buttonVariants({ variant: "ghost", size: "sm" })}
+            onClick={() => setOpen(!open)}
+          >
+            Export
           </div>
-          <div className="rounded-md  flex flex-grow">
-            <div className="rounded-md mb-2 flex flex-col flex-grow p-2 w-9 border-solid border border-gray-200">
-              <small>Output format</small>
-              <div>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 min-w[550px] w-[550px]">
+          <div className="p-4">
+            <div className="mb-2 font-bold">Export</div>
+            <div className="p-2 rounded-md mb-1 border-solid border border-gray-200">
+              <small>Export target</small>
+              <div className="p-2">
                 <RadioGroup
-                  defaultValue={format}
+                  defaultValue={exportTarget}
                   onValueChange={(e) => {
-                    setFormat(e as ExportFormat);
-                    if (e === "csv") {
-                      setExportOptions(csvDelimeter);
-                    } else if (e === "xlsx") {
-                      setExportOptions(excelDiliemter);
-                    } else if (e === "delimited") {
-                      setExportOptions(delimitedOptions);
-                    } else {
-                      setExportOptions(null);
-                    }
+                    setExportTarget(e as ExportTarget);
                   }}
                 >
-                  <div className="p-2">
-                    <div className="flex items-center space-x-2 pb-2">
-                      <RadioGroupItem value="csv" />
-                      <small>CSV</small>
-                    </div>
-                    <div className="flex items-center space-x-2 pb-2">
-                      <RadioGroupItem value="delimited" />
-                      <small>DELIMITED TEXT</small>
-                    </div>
-                    <div className="flex items-center space-x-2 pb-2">
-                      <RadioGroupItem value="json" />
-                      <small>JSON</small>
-                    </div>
-                    <div className="flex items-center space-x-2 pb-2">
-                      <RadioGroupItem value="sql" />
-                      <small>SQL</small>
+                  <div className="flex items-center  space-x-16">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="clipboard" />
+                      <small>Copy to clipboard</small>
                     </div>
                     <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="xlsx" />
-                      <small>EXCEL</small>
+                      <RadioGroupItem value="file" />
+                      <small>Export to file</small>
                     </div>
                   </div>
                 </RadioGroup>
               </div>
             </div>
-            <div>
-              <div className="rounded-md mb-2 flex flex-grow ml-2 p-2 flex-col border-solid border border-gray-200">
-                <small>Selection</small>
+            <div className="rounded-md  flex flex-grow">
+              <div className="rounded-md mb-2 flex flex-col flex-grow p-2 w-9 border-solid border border-gray-200">
+                <small>Output format</small>
                 <div>
                   <RadioGroup
-                    defaultValue={exportSelection}
+                    defaultValue={format}
                     onValueChange={(e) => {
-                      setExportSelection(e as ExportSelection);
+                      setFormat(e as ExportFormat);
+                      if (e === "csv") {
+                        setExportOptions(csvDelimeter);
+                      } else if (e === "xlsx") {
+                        setExportOptions(excelDiliemter);
+                      } else if (e === "delimited") {
+                        setExportOptions(delimitedOptions);
+                      } else {
+                        setExportOptions(null);
+                      }
                     }}
                   >
                     <div className="p-2">
                       <div className="flex items-center space-x-2 pb-2">
-                        <RadioGroupItem value="complete" />
-                        <small>
-                          Complete ({data.getAllRows().length} rows)
-                        </small>
+                        <RadioGroupItem value="csv" />
+                        <small>CSV</small>
                       </div>
                       <div className="flex items-center space-x-2 pb-2">
-                        <RadioGroupItem
-                          value="selected_row"
-                          disabled={selectionCount.rows === 0}
-                        />
-                        <small>Rows ({selectionCount.rows} rows)</small>
+                        <RadioGroupItem value="delimited" />
+                        <small>DELIMITED TEXT</small>
                       </div>
                       <div className="flex items-center space-x-2 pb-2">
-                        <RadioGroupItem
-                          value="selected_col"
-                          disabled={selectionCount.cols === 0}
-                        />
-                        <small>Columns ({selectionCount.cols} cols)</small>
+                        <RadioGroupItem value="json" />
+                        <small>JSON</small>
                       </div>
                       <div className="flex items-center space-x-2 pb-2">
-                        <RadioGroupItem
-                          value="selected_range"
-                          disabled={selectionCount.ranges.length === 0}
-                        />
-                        <small>Ranges</small>
-                        <div>
-                          {selectionCount.ranges.length > 0 &&
-                            SelectedRange({
-                              ranges: selectionCount.ranges,
-                              value:
-                                selectedRangeIndex >= 0
-                                  ? selectedRangeIndex.toString()
-                                  : "0",
-                              onChange: (value) => {
-                                setSelectedRangeIndex(parseInt(value));
-                              },
-                            })}
-                        </div>
+                        <RadioGroupItem value="sql" />
+                        <small>SQL</small>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="xlsx" />
+                        <small>EXCEL</small>
                       </div>
                     </div>
                   </RadioGroup>
                 </div>
               </div>
-              <div className="rounded-md mb-2 flex flex-grow ml-2 p-2 flex-col border-solid border border-gray-200">
-                <small>Options</small>
-                <div className="flex flex-col space-y-2">
-                  <div className="flex items-center space-x-4">
-                    <small className="w-[120px] text-right">
-                      Field separator:
-                    </small>
-                    <div className="flex items-center rounded-md bg-white px-3 py-2.5 text-base text-neutral-900 outline outline-1 outline-neutral-200 focus:outline-neutral-400/70 dark:bg-neutral-900 dark:text-white dark:outline-neutral-800 dark:focus:outline-neutral-600 h-[28px] w-[120px]">
-                      <input
-                        disabled={format !== "delimited"}
-                        type="text"
-                        className="bg-transparent flex-1 outline-none text-sm font-light"
-                        value={exportOptions?.fieldSeparator || ""}
-                        onChange={(e) => {
-                          setExportOptions({
-                            ...exportOptions,
-                            fieldSeparator: e.target.value,
-                          });
-                        }}
-                      />
-                    </div>
+              <div>
+                <div className="rounded-md mb-2 flex flex-grow ml-2 p-2 flex-col border-solid border border-gray-200">
+                  <small>Selection</small>
+                  <div>
+                    <RadioGroup
+                      defaultValue={exportSelection}
+                      onValueChange={(e) => {
+                        setExportSelection(e as ExportSelection);
+                      }}
+                    >
+                      <div className="p-2">
+                        <div className="flex items-center space-x-2 pb-2">
+                          <RadioGroupItem value="complete" />
+                          <small>
+                            Complete ({data.getAllRows().length} rows)
+                          </small>
+                        </div>
+                        <div className="flex items-center space-x-2 pb-2">
+                          <RadioGroupItem
+                            value="selected_row"
+                            disabled={selectionCount.rows === 0}
+                          />
+                          <small>Rows ({selectionCount.rows} rows)</small>
+                        </div>
+                        <div className="flex items-center space-x-2 pb-2">
+                          <RadioGroupItem
+                            value="selected_col"
+                            disabled={selectionCount.cols === 0}
+                          />
+                          <small>Columns ({selectionCount.cols} cols)</small>
+                        </div>
+                        <div className="flex items-center space-x-2 pb-2">
+                          <RadioGroupItem
+                            value="selected_range"
+                            disabled={selectionCount.ranges.length === 0}
+                          />
+                          <small>Ranges</small>
+                          <div>
+                            {selectionCount.ranges.length > 0 &&
+                              SelectedRange({
+                                ranges: selectionCount.ranges,
+                                value:
+                                  selectedRangeIndex >= 0
+                                    ? selectedRangeIndex.toString()
+                                    : "0",
+                                onChange: (value) => {
+                                  setSelectedRangeIndex(parseInt(value));
+                                },
+                              })}
+                          </div>
+                        </div>
+                      </div>
+                    </RadioGroup>
                   </div>
-                  <div className="flex items-center space-x-4">
-                    <small className="w-[120px] text-right">
-                      Line terminator:
-                    </small>
-                    <div className="flex items-center rounded-md bg-white px-3 py-2.5 text-base text-neutral-900 outline outline-1 outline-neutral-200 focus:outline-neutral-400/70 dark:bg-neutral-900 dark:text-white dark:outline-neutral-800 dark:focus:outline-neutral-600 h-[28px] w-[120px]">
-                      <input
-                        disabled={format !== "delimited"}
-                        type="text"
-                        className="bg-transparent flex-1 outline-none text-sm font-light"
-                        value={exportOptions?.lineTerminator || ""}
-                        onChange={(e) => {
-                          setExportOptions({
-                            ...exportOptions,
-                            lineTerminator: e.target.value,
-                          });
-                        }}
-                      />
+                </div>
+                <div className="rounded-md mb-2 flex flex-grow ml-2 p-2 flex-col border-solid border border-gray-200">
+                  <small>Options</small>
+                  <div className="flex flex-col space-y-2">
+                    <div className="flex items-center space-x-4">
+                      <small className="w-[120px] text-right">
+                        Field separator:
+                      </small>
+                      <div className="flex items-center rounded-md bg-white px-3 py-2.5 text-base text-neutral-900 outline outline-1 outline-neutral-200 focus:outline-neutral-400/70 dark:bg-neutral-900 dark:text-white dark:outline-neutral-800 dark:focus:outline-neutral-600 h-[28px] w-[120px]">
+                        <input
+                          disabled={format !== "delimited"}
+                          type="text"
+                          className="bg-transparent flex-1 outline-none text-sm font-light"
+                          value={exportOptions?.fieldSeparator || ""}
+                          onChange={(e) => {
+                            setExportOptions({
+                              ...exportOptions,
+                              fieldSeparator: e.target.value,
+                            });
+                          }}
+                        />
+                      </div>
                     </div>
-                  </div>
+                    <div className="flex items-center space-x-4">
+                      <small className="w-[120px] text-right">
+                        Line terminator:
+                      </small>
+                      <div className="flex items-center rounded-md bg-white px-3 py-2.5 text-base text-neutral-900 outline outline-1 outline-neutral-200 focus:outline-neutral-400/70 dark:bg-neutral-900 dark:text-white dark:outline-neutral-800 dark:focus:outline-neutral-600 h-[28px] w-[120px]">
+                        <input
+                          disabled={format !== "delimited"}
+                          type="text"
+                          className="bg-transparent flex-1 outline-none text-sm font-light"
+                          value={exportOptions?.lineTerminator || ""}
+                          onChange={(e) => {
+                            setExportOptions({
+                              ...exportOptions,
+                              lineTerminator: e.target.value,
+                            });
+                          }}
+                        />
+                      </div>
+                    </div>
 
-                  <div className="flex items-center space-x-4">
-                    <small className="w-[120px] text-right">Encloser:</small>
-                    <div className="flex items-center rounded-md bg-white px-3 py-2.5 text-base text-neutral-900 outline outline-1 outline-neutral-200 focus:outline-neutral-400/70 dark:bg-neutral-900 dark:text-white dark:outline-neutral-800 dark:focus:outline-neutral-600 h-[28px] w-[120px]">
-                      <input
-                        disabled={format !== "delimited"}
-                        type="text"
-                        className="bg-transparent flex-1 outline-none text-sm font-light"
-                        value={exportOptions?.encloser || ""}
-                        onChange={(e) => {
-                          setExportOptions({
-                            ...exportOptions,
-                            encloser: e.target.value,
-                          });
-                        }}
-                      />
+                    <div className="flex items-center space-x-4">
+                      <small className="w-[120px] text-right">Encloser:</small>
+                      <div className="flex items-center rounded-md bg-white px-3 py-2.5 text-base text-neutral-900 outline outline-1 outline-neutral-200 focus:outline-neutral-400/70 dark:bg-neutral-900 dark:text-white dark:outline-neutral-800 dark:focus:outline-neutral-600 h-[28px] w-[120px]">
+                        <input
+                          disabled={format !== "delimited"}
+                          type="text"
+                          className="bg-transparent flex-1 outline-none text-sm font-light"
+                          value={exportOptions?.encloser || ""}
+                          onChange={(e) => {
+                            setExportOptions({
+                              ...exportOptions,
+                              encloser: e.target.value,
+                            });
+                          }}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="p-2 pt-0 px-4">
-          <Button size="sm" onClick={onExportClicked}>
-            Export
-          </Button>
-        </div>
-      </PopoverContent>
-    </Popover>
+          <div className="p-2 pt-0 px-4">
+            <Button size="sm" onClick={onExportClicked}>
+              Export
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
 
