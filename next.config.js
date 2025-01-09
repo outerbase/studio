9c7b74f5e7e1b7473ab10caf3abccb435844e30d
Visal .In <invisal@gmail.com>
2025-01-09 09:19:37 +0700

    adding cors for event tracking

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         source: "/api/events",
         headers: [
           { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
