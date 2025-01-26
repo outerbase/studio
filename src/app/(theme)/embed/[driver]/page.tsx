eaa1a03a5938e77066eaba49fb4e0f63364135a5
Visal .In <invisal@gmail.com>
2025-01-26 16:48:03 +0700

    refactor embed page to single page

---
+++ src/app/(theme)/embed/[driver]/page.tsx
@@ -0,0 +1,48 @@
+import ClientOnly from "@/components/client-only";
+import ThemeLayout from "../../theme_layout";
+import EmbedPageClient from "./page-client";
+
+export interface EmbedPageProps {
+  searchParams: Promise<{
+    theme?: string;
+    disableThemeToggle?: string;
+    [key: string]: any;
+  }>;
+  params: Promise<{
+    driver: string;
+  }>;
+}
+
+export default async function EmbedPage(props: EmbedPageProps) {
+  const searchParams = await props.searchParams;
+  const driver = (await props.params).driver;
+
+  let overrideTheme: "dark" | "light" | undefined = undefined;
+  const disableToggle = searchParams.disableThemeToggle === "1";
+
+  if (searchParams.theme) {
+    overrideTheme = searchParams.theme === "dark" ? "dark" : "light";
+  }
+
+  const overrideThemeVariables: Record<string, string> = {};
+
+  for (const key in searchParams) {
+    if (!key.startsWith("themeVariables[")) {
+      continue;
+    }
+
+    overrideThemeVariables[key.slice(15, -1)] = searchParams[key];
+  }
+
+  return (
+    <ThemeLayout
+      overrideTheme={overrideTheme}
+      disableToggle={disableToggle}
+      overrideThemeVariables={overrideThemeVariables}
+    >
+      <ClientOnly>
+        <EmbedPageClient driverName={driver} />
+      </ClientOnly>
+    </ThemeLayout>
+  );
+}
