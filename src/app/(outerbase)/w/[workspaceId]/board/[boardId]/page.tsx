8a2b1f04165ae3e8140f2aa4e7e948e3a7a69848
Visal .In <invisal@gmail.com>
2025-02-02 22:02:12 +0700

    try to use outerbase board data with our board

--- src/app/(outerbase)/w/[workspaceId]/board/[boardId]/page.tsx
+++ src/app/(outerbase)/w/[workspaceId]/board/[boardId]/page.tsx
@@ -1,19 +1,42 @@
-import ClientOnly from "@/components/client-only";
-import BoardPageClient from "./page-client";
+"use client";
 
-interface BoardPageProps {
-  params: Promise<{ workspaceId: string; boardId: string }>;
+import Board from "@/components/board";
+import ClientOnly from "@/components/client-only";
+import { getOuterbaseDashboard } from "@/outerbase-cloud/api";
+import { OuterbaseAPIDashboardDetail } from "@/outerbase-cloud/api-type";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import useSWR from "swr";
+
+function BoardPageEditor({
+  initialValue,
+}: {
+  initialValue: OuterbaseAPIDashboardDetail;
+}) {
+  console.log(initialValue);
+  const [value, setValue] = useState(initialValue);
+
+  return <Board value={value as any} setValue={setValue as any} />;
 }
 
-export default async function BoardPage(props: BoardPageProps) {
-  const params = await props.params;
+export default function BoardPage() {
+  const { workspaceId, boardId } = useParams<{
+    boardId: string;
+    workspaceId: string;
+  }>();
+  const { data } = useSWR(`board-${boardId}`, () => {
+    return getOuterbaseDashboard(workspaceId, boardId);
+  });
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <ClientOnly>
-      <BoardPageClient
-        workspaceId={params.workspaceId}
-        boardId={params.boardId}
-      />
-    </ClientOnly>
+    <div className="h-screen w-screen overflow-hidden">
+      <ClientOnly>
+        <BoardPageEditor initialValue={data} />
+      </ClientOnly>
+    </div>
   );
 }
