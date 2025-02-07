cdbd72dcf2d23f9ceba4c9ddb5072ea803d96682
keppere <50957820+keppere@users.noreply.github.com>
2025-02-07 13:50:26 +0700

    Chart editor (#343)
    * feat: chart beta
    
    * refact: change theme provider
    
    * refact: remove unused chart page and sidebar menu item
    
    * refact: enhance chart options builder and improve theme handling
    
    * refact: simplify chart editor state management and add simple combobox component
    
    * refact: rename chart types and options builder files, add simple input and toggle components
    
    * prevent flicking
    
    * feat: handle chart series
    * Add more series
    * Change series
    * Remove
    
    * feat: update chart editor options and improve text color handling
    
    * feat: add color picker to chart series combobox and update color handling
    
    * feat: enhance chart button styles with new color variables
    
    * feat: add background selection component with gradient options to chart
    
    ---------
    
    Co-authored-by: Visal .In <invisal@gmail.com>

--- src/components/chart/simple-input.tsx
+++ src/components/chart/simple-input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface SimpleInputProps {
   value?: string;
@@ -13,9 +13,13 @@ export default function SimpleInput({
 }: SimpleInputProps) {
   const [text, setText] = React.useState(value || "");
 
+  useEffect(() => {
+    setText(value || "");
+  }, [value]);
+
   return (
     <input
-      className="w-full rounded-md border p-2"
+      className="h-[36px] w-full rounded-md border p-2"
       placeholder={placeholder ?? ""}
       value={text ?? ""}
       onChange={(v) => {
