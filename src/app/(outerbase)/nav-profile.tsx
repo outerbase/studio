9689981a45d0bf5ec803bee86c28e70ca99352f7
Visal .In <invisal@gmail.com>
2025-02-09 08:13:38 +0700

    add toggle menu on the nav profile

--- src/app/(outerbase)/nav-profile.tsx
+++ src/app/(outerbase)/nav-profile.tsx
@@ -6,13 +6,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Gear, SignOut, ToggleLeft, ToggleRight } from "@phosphor-icons/react";
+import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import { useSession } from "./session-provider";
 
 export default function NavigationProfile() {
+  const { resolvedTheme, forcedTheme, setTheme } = useTheme();
   const { session } = useSession();
   const router = useRouter();
+
+  const theme = forcedTheme ?? resolvedTheme;
 
   const onLogoutClicked = useCallback(() => {
     // Remove all the session data
@@ -21,10 +26,21 @@ export default function NavigationProfile() {
     router.push("/signin");
   }, [router]);
 
+  const onThemeToggleClicked = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      setTheme(theme === "dark" ? "light" : "dark");
+
+      // We don't want the dropdown to close
+      e.stopPropagation();
+      e.preventDefault();
+    },
+    [theme, setTheme]
+  );
+
   if (!session?.user) return null;
 
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger>
         <Avatar size="lg" username={session.user.initials} as="div" />
       </DropdownMenuTrigger>
@@ -40,9 +56,24 @@ export default function NavigationProfile() {
           </div>
         </div>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Account Setting</DropdownMenuItem>
-        <DropdownMenuItem>Theme</DropdownMenuItem>
-        <DropdownMenuItem onClick={onLogoutClicked}>Log out</DropdownMenuItem>
+        <DropdownMenuItem className="justify-between">
+          Account Setting
+          <Gear size={20} />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="justify-between"
+          onClick={onThemeToggleClicked}
+        >
+          Theme
+          {theme === "dark" ? (
+            <ToggleRight weight="fill" size={20} />
+          ) : (
+            <ToggleLeft size={20} />
+          )}
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={onLogoutClicked} className="justify-between">
+          Log out <SignOut size={20} />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
