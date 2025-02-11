5b45cb6f2f1dcbc42f9e24599d8d9f3223732480
roth-dev <roth.dev.ops@gmail.com>
2025-02-11 21:50:58 +0700

    account deletion confirmation footer component

--- src/app/(outerbase)/w/[workspaceId]/account/page.tsx
+++ src/app/(outerbase)/w/[workspaceId]/account/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { NavigationBar } from "@/app/(outerbase)/nav";
 import { useSession } from "@/app/(outerbase)/session-provider";
-import { Button } from "@/components/orbit/button";
+import AccountFooter from "./account-footer";
 import TwoFactorAuth from "./two-factor-auth";
 import UserAvatar from "./user-avatar";
 import UserFormInput from "./user-form-input";
@@ -12,33 +12,19 @@ export default function AccountPage() {
   if (!session?.user) return <div>You are not login!</div>;
 
   return (
-    <div className="bg-neutral-50 dark:bg-neutral-950">
+    <div className="min-h-screen bg-neutral-50 dark:bg-neutral-950">
       <NavigationBar />
 
-      <main className="mx-auto mt-20 mb-32 flex max-w-sm flex-col">
+      <main className="mx-auto flex max-w-sm flex-col overflow-x-hidden overflow-y-auto pt-20 pb-10">
         <div className="flex flex-col gap-5">
-          <h1 className="text-primary text-xl font-bold">Account Setting</h1>
+          <h1 className="text-primary text-2xl font-bold">Account Setting</h1>
           <h2 className="mt-5 text-xl font-medium text-balance text-neutral-800 dark:text-neutral-100">
             Details
           </h2>
-
           <UserAvatar user={session.user} />
           <UserFormInput user={session.user} />
           <TwoFactorAuth />
-        </div>
-        <div className="flex flex-col items-start gap-2">
-          <h2 className="mt-5 text-xl font-medium text-balance text-neutral-800 dark:text-neutral-100">
-            Delete Account
-          </h2>
-          <div className="text-base">
-            This will delete your account and all the workspaces you own.
-          </div>
-          <Button
-            className="mt-5"
-            title="Delete Account"
-            variant="destructive"
-            size="lg"
-          />
+          <AccountFooter />
         </div>
       </main>
     </div>
