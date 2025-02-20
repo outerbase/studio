1a1be03955bf2f21c5452d7c7f02e239f41fbc48
sokphaladam <sokphaladam@gmail.com>
2025-02-20 12:15:05 +0700

    Sign up outerbase

--- src/outerbase-cloud/api-type.ts
+++ src/outerbase-cloud/api-type.ts
@@ -120,6 +120,7 @@ export interface OuterbaseAPISession {
   oauth_verified_at: string | null;
   expires_at: string | null;
   token: string;
+  email_verified_at: string | null;
 }
 
 export interface OuterbaseAPIUser {
