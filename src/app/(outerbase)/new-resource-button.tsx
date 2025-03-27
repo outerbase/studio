947782f4da2031931336671fbf97b6158ec91754
Visal .In <invisal@gmail.com>
2025-03-27 15:20:29 +0700

    Add durable object instruction using browsable durable object package

--- src/app/(outerbase)/new-resource-button.tsx
+++ src/app/(outerbase)/new-resource-button.tsx
@@ -20,11 +20,10 @@ function CreateResourceItem({
       <div
         className={cn(
           "text-muted-foreground flex w-full cursor-pointer items-center justify-start rounded p-2 text-base",
-          { "bg-secondary": selected },
-          data.colorClassName
+          { "bg-secondary": selected }
         )}
       >
-        <data.icon className="mr-2 h-6 w-6" />
+        <data.icon className={cn("mr-2 h-6 w-6", data.colorClassName)} />
         <span className="flex-1 text-left">{data.name}</span>
         {data.comingSoon && (
           <span className="text-primary-foreground rounded border bg-zinc-500 px-1.5 py-0.5 text-xs">
@@ -41,11 +40,10 @@ function CreateResourceItem({
       href={data.href}
       className={cn(
         "hover:bg-secondary flex w-full cursor-pointer items-center justify-start rounded p-2 text-base",
-        { "bg-secondary": selected },
-        data.colorClassName
+        { "bg-secondary": selected }
       )}
     >
-      <data.icon className="mr-2 h-6 w-6" />
+      <data.icon className={cn("mr-2 h-6 w-6", data.colorClassName)} />
       <span className="flex-1 text-left">{data.name}</span>
     </Link>
   );
@@ -57,7 +55,7 @@ export interface NewResourceType {
   href: string;
   comingSoon?: boolean;
   colorClassName?: string;
-  thirdParty?: boolean;
+  cloudflare?: boolean;
 }
 interface NewResourceProps {
   onCreateBoard?: () => void;
@@ -77,6 +75,8 @@ export default function NewResourceType({
   const [search, setSearch] = useState("");
   const [open, setOpen] = useState(false);
 
+  const cloudflare = resourceTypeList.filter((resource) => resource.cloudflare);
+
   const allResourceType = (
     <>
       <div className="flex gap-4 p-2">
@@ -90,72 +90,33 @@ export default function NewResourceType({
             <div className="text-sm">Multiple source dashboard</div>
           </div>
         </button>
-
-        {/* <button className="flex w-[200px] items-center gap-4 rounded bg-gradient-to-r from-blue-800 to-indigo-900 p-2 px-4 text-left text-base text-white">
-          <StarbaseIcon className="h-8 w-8" />
-          <div>
-            <div className="font-semibold">StarbaseDB</div>
-            <div className="text-sm">Free 10GB SQLite</div>
-          </div>
-        </button> */}
       </div>
 
       <h2 className="px-3 py-2 text-base font-semibold">
         Bring your existing databases
       </h2>
 
-      <div className="my-2 flex">
-        <div className="flex w-1/2 flex-col gap-2 px-2">
-          {resourceTypeList
-            .filter((resource) => !resource.thirdParty)
-            .filter((_, idx) => idx % 2 === 0)
-            .map((resource) => (
-              <CreateResourceItem key={resource.name} data={resource} />
-            ))}
-        </div>
-
-        <div className="flex w-1/2 flex-col gap-2 px-2">
-          {resourceTypeList
-            .filter((resource) => !resource.thirdParty)
-            .filter((_, idx) => idx % 2 === 1)
-            .map((resource) => (
-              <CreateResourceItem key={resource.name} data={resource} />
-            ))}
-        </div>
+      <div className="my-2 grid grid-cols-2 gap-2 px-2">
+        {resourceTypeList
+          .filter((resource) => !resource.cloudflare)
+          .map((resource) => (
+            <CreateResourceItem key={resource.name} data={resource} />
+          ))}
       </div>
 
-      <h2 className="border-t px-3 py-2 pt-4 text-base font-semibold">
-        Third party integrations
-      </h2>
+      {cloudflare.length > 0 && (
+        <>
+          <div className="text-muted-foreground mt-4 border-t-4 p-4 text-sm font-semibold tracking-wider">
+            CLOUDFLARE
+          </div>
 
-      <div className="my- flex">
-        <div className="flex w-1/2 flex-col gap-2 px-2">
-          {resourceTypeList
-            .filter((resource) => !!resource.thirdParty)
-            .filter((_, idx) => idx % 2 === 0)
-            .map((resource) => (
+          <div className="mb-2 grid grid-cols-2 gap-2 px-2">
+            {cloudflare.map((resource) => (
               <CreateResourceItem key={resource.name} data={resource} />
             ))}
-        </div>
-
-        <div className="flex w-1/2 flex-col gap-2 px-2">
-          {resourceTypeList
-            .filter((resource) => !!resource.thirdParty)
-            .filter((_, idx) => idx % 2 === 1)
-            .map((resource) => (
-              <CreateResourceItem key={resource.name} data={resource} />
-            ))}
-        </div>
-      </div>
-
-      {/* <h2 className="mt-4 px-3 py-1 text-base">Or use connection string</h2>
-      <div className="mx-2 my-2 flex">
-        <Input
-          className="bg-secondary w-full"
-          postText={<ArrowLeft className="mr-2" />}
-          placeholder="postgres://testing"
-        />
-      </div> */}
+          </div>
+        </>
+      )}
     </>
   );
 
